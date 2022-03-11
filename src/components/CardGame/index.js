13f3ad27048650f6232815030efc35e024b5997c
Wérikis Xavier <50964201+werikisxavier@users.noreply.github.com>
2022-03-11 11:37:43 -0300

    :tada: Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+
+function CardGame(){
+    return `
+    <article class= "card-game">
+        <img src="src/images/alura-pixel.png" alt="Logo da Alura">
+    </article>
+    `
+    
+}
+
+export default CardGame;
