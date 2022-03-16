796e222fab384217de334a0e3c2742aa7a12869a
Wérikis Xavier <50964201+werikisxavier@users.noreply.github.com>
2022-03-16 11:04:03 -0300

    :construction: Update Style CSS files

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ import "./src/styles/generic/reset.css"
 import "./src/styles/elements/base.css"
 
 import BoardGame from "./src/objects/BoardGame";
-import PlayerName from "./src/components/PlayerName";
+import ScoreBoard from "./src/objects/ScoreBoard";
 
 const $root = document.querySelector("#root");
 
 $root.insertAdjacentHTML("beforeend",
 `
-${PlayerName("Player1")}
-${PlayerName("Player2")}
+${ScoreBoard("Player1","Player2")}
 ${BoardGame(6)}
 
 `);
